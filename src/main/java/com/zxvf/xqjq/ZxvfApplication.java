25cb4976810ebdc3a58d985f55baf0aa89525918
shilc <15733206393@163.com>
2019-05-05 21:21:33 +0800

    spring

---
+++ src/main/java/com/zxvf/xqjq/ZxvfApplication.java
@@ -0,0 +1,13 @@
+package com.zxvf.xqjq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZxvfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZxvfApplication.class, args);
+    }
+
+}
